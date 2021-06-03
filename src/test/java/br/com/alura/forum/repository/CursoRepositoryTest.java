6e6a5bb8bf00921bf6b8a7fd59da0bf93f6f5173
Cassio <cassioate@gmail.com>
2021-06-03 19:17:55 -0300

    Testes com Junit e Spring Boot utilizando H2 para o repository

---
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -0,0 +1,36 @@
+package br.com.alura.forum.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.alura.forum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository repository;
+
+	@Test
+	void deveriaCarregarUmCursoAoBuscarPeloNome() {
+		String nomeCurso = "HTML 5";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals(nomeCurso, curso.getNome());
+	}
+
+	@Test
+	void naoDeveriaCarregarUmCursoAoBuscarPeloNome() {
+		String nomeCurso = "HTML 1";
+		Curso curso = repository.findByNome(nomeCurso);
+		Assert.assertNull(curso);
+	}
+}
